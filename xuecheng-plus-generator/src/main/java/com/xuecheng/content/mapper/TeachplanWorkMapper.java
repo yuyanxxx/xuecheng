ddc23eb9ceef560fffac5f308e02c10dbe91baaf
tanjian <2971543391@qq.com>
2023-06-12 17:32:34 +0800

    内容模型搭建

---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/content/mapper/TeachplanWorkMapper.java
@@ -0,0 +1,16 @@
+package com.xuecheng.content.mapper;
+
+import com.xuecheng.content.model.po.TeachplanWork;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface TeachplanWorkMapper extends BaseMapper<TeachplanWork> {
+
+}
---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/content/service/TeachplanWorkService.java
@@ -0,0 +1,16 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.content.model.po.TeachplanWork;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author itcast
+ * @since 2023-06-12
+ */
+public interface TeachplanWorkService extends IService<TeachplanWork> {
+
+}
